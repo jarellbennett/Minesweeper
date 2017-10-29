1452cc8c67bb8968a67a4b575ff212eb5979ad19
Jarell <jb123506@gmail.com>
2017-10-28 23:51:11 -0400

    created function generatebombboard

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -5,9 +5,38 @@ for(i = 0; i<numberOfRows; i++ ){
     let row = [];    //row for game board
 
     for(j=0; j< numberOfColumns; j++){
+      row.push(null);
+    }
+    board.push(row);   //adds newly formed row to game board
+  }
+  return board;
+};
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) =>{
+  let board = [];    //will represent the game board
+
+for(i = 0; i<numberOfRows; i++ ){
+    let row = [];    //row for game board
+
+    for(j=0; j< numberOfColumns; j++){
       row.push(' ');
     }
     board.push(row);   //adds newly formed row to game board
+  }
+
+  let numberOfBombsPlaced = 0;    //bomb counter
+
+//while loop that places bombs randomlly on game board
+  while(numberOfBombsPlaced < numberOfBombs){
+    //random number generator for row index
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    //random number generator fot the colums
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    //place bomb at spot from random indices
+    board[randomRowIndex,randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+    //important note: The code in this while loop has the potential to place 
+    //bombs on top of already existing bombs.
   }
   return board;
 }
